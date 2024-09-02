7c1d05dd35d2ecce66084adcf8d11d45170f8e77
dhopegraphics <dhopegraphics@gmail.com>
2024-09-02 17:52:18 +0000

    width adjustment fixed

--- components/SymptomsComponent.jsx
+++ components/SymptomsComponent.jsx
@@ -1,15 +1,15 @@
-import { View, Text } from "react-native";
+import { View, Text, TouchableOpacity } from "react-native";
 import React from "react";
-import { TouchableOpacity } from "react-native";
 
 const SymptomsComponent = ({ Symptom, ButtonColor2, TextColor }) => {
   return (
     <View>
-      <View className="flex-row flex-1  ">
+      <View className="flex-row flex-1">
         <TouchableOpacity
-          className={`rounded-2xl ${ButtonColor2} w-28  max-w-full h-12 items-center justify-center  m-2`}
+          className={`rounded-2xl ${ButtonColor2} h-12 items-center justify-center m-2`}
+          style={{ minWidth: 70, paddingHorizontal: 12 }} // Dynamic width styling
         >
-          <Text className={` ${TextColor} font-JakartaSemiBold text-sm`}>
+          <Text className={`${TextColor} font-JakartaSemiBold text-sm`}>
             {Symptom}
           </Text>
         </TouchableOpacity>
